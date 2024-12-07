5d47a9dafad4fda86d9778a4d333ac92736a4998
Michal Jakubík <525062@mail.muni.cz>
2024-12-07 20:10:21 +0100

    fix: removed accuracy computation in script

--- run.sh
+++ run.sh
@@ -15,13 +15,8 @@ echo "#################"
 
 nice -n 19 src/main
 
-echo "train accuracy:"
-python3 evaluator/evaluate.py train_predictions.csv data/fashion_mnist_train_labels.csv
+# echo "train accuracy:"
+# python3 evaluator/evaluate.py train_predictions.csv data/fashion_mnist_train_labels.csv
 
-echo "test accuracy:"
-python3 evaluator/evaluate.py test_predictions.csv data/fashion_mnist_test_labels.csv
-
-## use nice to decrease priority in order to comply with aisa rules
-## https://www.fi.muni.cz/tech/unix/computation.html.en
-## especially if you are using multiple cores
-# nice -n 19 ./network
+# echo "test accuracy:"
+# python3 evaluator/evaluate.py test_predictions.csv data/fashion_mnist_test_labels.csv
