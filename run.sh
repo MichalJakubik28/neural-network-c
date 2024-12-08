1a48f9e667dd8c8136d32636457d5f193f9731ed
Michal Jakubík <xjakubi2@fi.muni.cz>
2024-12-08 23:34:12 +0100

    fix: add gcc module adding

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
+echo "Adding gcc..."
+module add gcc
+if [ $? -eq 0 ]; then echo "Imported gcc."; fi;
 echo "#################"
 echo "    COMPILING    "
 echo "#################"
 
 gcc -fopenmp -O3 -Wall src/main.c -o src/main src/utils/csv_to_img.c src/matrix/matrix.c src/weight_ops/weight_ops.c src/network/network.c -lm
-
+if [ $? -eq 0 ]; then echo "Compilation successful."; fi;
 echo "#################"
 echo "     RUNNING     "
 echo "#################"
